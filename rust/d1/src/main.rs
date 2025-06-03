642c5450ff6c0ac7b4fa73fe39d411e8441ad659
Ilan <ilan.pillemer@gmail.com>
2025-06-03 14:07:05 +0100

    day 1 part 2

--- rust/d1/src/main.rs
+++ rust/d1/src/main.rs
@@ -1,8 +1,8 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-
 // https://adventofcode.com/2024/day/1
 // correct day 1 part 1 answer is 1660292 for my input
 
@@ -11,6 +11,7 @@ fn main() -> io::Result<()> {
     let f = BufReader::new(f);
     let mut v = Vec::new();
     let mut w = Vec::new();
+    let mut counts = HashMap::<i32, i32>::new();
     for line in f.lines() {
         let line = line?;
         let mut xs = line.split_whitespace();
@@ -18,14 +19,24 @@ fn main() -> io::Result<()> {
         let right: i32 = xs.next().expect("").trim().parse().expect("");
         v.push(left);
         w.push(right);
+
+        // build the distribution for part 2
+        counts.entry(left).or_insert(0);
+        let count = counts.entry(right).or_insert(0);
+        *count += 1
     }
 
     v.sort();
     w.sort();
 
     let total: i32 = v.iter().zip(w).map(|(a, b)| (a - b).abs()).sum();
+    let score: i32 = v.iter().map(|x| x * counts.get(x).expect("")).sum();
 
     assert_eq!(total, 1660292); // only true for my input
     println!("Day 1, Part 1: {total}");
+
+    assert_eq!(score, 22776016); // only true for my input
+    println!("Day 1, Part 2: {score}");
+
     Ok(())
 }
